df5a4ad9dfbb82816976fa8e9660a9aa06cd6388
Ankit Kumar <ak21csb0b04@student.nitw.ac.in>
2024-06-13 13:45:00 +0530

    Create pages

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,29 @@
+import { BrowserRouter ,Routes ,Route} from 'react-router-dom'
 import React from 'react'
+import About from './pages/About'
+import Projects from './pages/Projects'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
 
-export default function>Main() {
+export default function Main() {
   return (
-    <div>main</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/Home" element = {<Home />} />
+        <Route path="/About" element = {<About />} />
+        <Route path="/Projects" element = {<Projects />} />
+        <Route path="/Dashboard" element = {<Dashboard />} />
+        <Route path="/sign-in" element = {<Signin />} />
+        <Route path="/Sign-up" element = {<Signup />} />
+
+
+
+
+
+
+      </Routes>
+    </BrowserRouter>
   )
 }

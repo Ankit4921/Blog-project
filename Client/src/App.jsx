c02f2f9b8680b50add029e839de9500d4c3dbae6
Ankit Kumar <ak21csb0b04@student.nitw.ac.in>
2024-06-20 12:40:36 +0530

    navbar link activated

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -6,24 +6,22 @@ import Signin from './pages/Signin'
 import Signup from './pages/Signup'
 import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
+import Header from './Components/Header'
 
 export default function Main() {
   return (
     <BrowserRouter>
-      <Routes>
+     <Header/>
+       <Routes>
         <Route path="/Home" element = {<Home />} />
         <Route path="/About" element = {<About />} />
         <Route path="/Projects" element = {<Projects />} />
         <Route path="/Dashboard" element = {<Dashboard />} />
         <Route path="/sign-in" element = {<Signin />} />
         <Route path="/Sign-up" element = {<Signup />} />
-
-
-
-
-
-
-      </Routes>
+ 
+       </Routes>
+     
     </BrowserRouter>
   )
 }
